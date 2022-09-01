1f0d26b577940c7b78e46608ec77397656bf0ab1
carlospb <carlospb@fceia.unr.edu.ar>
2022-09-01 13:59:57 -0300

    UPDATE - Bootstrap 5.2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,3 +7,11 @@ console.log('\'Allo \'Allo!');
 // Uncomment to enable Bootstrap popovers
 // https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
 // $(function () { $('[data-toggle="popover"]').popover(); });
+
+let i = 0;
+
+function getItem() { i++; console.log(i, 'GET_ITEM'); console.log(new Date());  }
+function getTagTrend() { console.log('GET_TREND'); console.log(new Date()); }
+
+let getTagTrendTimer = setTimeout(function myTimer() { getTagTrend(); getTagTrendTimer = setTimeout(myTimer, 30000);}, 30000);
+let getItemTimer = setTimeout(function myTimer() { getItem(); getItemTimer = setTimeout(myTimer, 5000); }, 5000);
